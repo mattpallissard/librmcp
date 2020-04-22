db7c986feccba6a696dbedcbaedf700be7d716cf
matt@pallissard.net <matt@pallissard.net>
2020-04-21 21:03:40 -0700

    correct date

--- src/test.c
+++ src/test.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010 matt pallissard
+Copyright (c) 2020 matt pallissard
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
